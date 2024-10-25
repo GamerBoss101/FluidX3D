cea9389877cbbff4a04e52694896be1bd095e92b
GamerBoss101 <76974209+GamerBoss101@users.noreply.github.com>
2024-10-25 16:58:10 -0400

    HyperNova STL CFD Configuration

--- src/setup.cpp
+++ src/setup.cpp
@@ -1,39 +1,69 @@
 #include "setup.hpp"
+#include "info.hpp";
 
-
-
-#ifdef BENCHMARK
-#include "info.hpp"
-void main_setup() { // benchmark; required extensions in defines.hpp: BENCHMARK, optionally FP16S or FP16C
+void main_setup() { // radial fan; required extensions in defines.hpp: FP16S, MOVING_BOUNDARIES, SUBGRID, INTERACTIVE_GRAPHICS or GRAPHICS
 	// ################################################################## define simulation box size, viscosity and volume force ###################################################################
-	uint mlups = 0u; {
+	const uint3 lbm_N = resolution(float3(3.0f, 3.0f, 1.0f), 181u); // input: simulation box aspect ratio and VRAM occupation in MB, output: grid resolution
+	const float lbm_Re = 100000.0f;
+	const float lbm_u = 0.1f;
+	const ulong lbm_T = 48000ull;
+	const ulong lbm_dt = 10ull;
+	LBM lbm(lbm_N, 1u, 1u, 1u, units.nu_from_Re(lbm_Re, (float)lbm_N.x, lbm_u));
+	// ###################################################################################### define geometry ######################################################################################
+	const float radius = 0.25f*(float)lbm_N.x;
+	const float3 center = float3(lbm.center().x, lbm.center().y, 0.36f*radius);
+	const float lbm_omega=lbm_u/radius, lbm_domega=lbm_omega*lbm_dt;
+	Mesh* mesh = read_stl(get_exe_path()+"../stl/FAN_Solid_Bottom.stl", lbm.size(), center, 2.0f*radius); // https://www.thingiverse.com/thing:6113/files
+	const uint Nx=lbm.get_Nx(), Ny=lbm.get_Ny(), Nz=lbm.get_Nz(); parallel_for(lbm.get_N(), [&](ulong n) { uint x=0u, y=0u, z=0u; lbm.coordinates(n, x, y, z);
+		if(x==0u||x==Nx-1u||y==0u||y==Ny-1u||z==0u) lbm.flags[n] = TYPE_S; // all non periodic
+	}); // ####################################################################### run simulation, export images and data ##########################################################################
+	lbm.graphics.visualization_modes = VIS_FLAG_LATTICE|VIS_FLAG_SURFACE|VIS_Q_CRITERION;
+	lbm.run(0u, lbm_T); // initialize simulation
+	while(lbm.get_t()<lbm_T) { // main simulation loop
+		lbm.voxelize_mesh_on_device(mesh, TYPE_S, center, float3(0.0f), float3(0.0f, 0.0f, lbm_omega));
+		lbm.run(lbm_dt, lbm_T);
+		mesh->rotate(float3x3(float3(0.0f, 0.0f, 1.0f), lbm_domega)); // rotate mesh
+		#if defined(GRAPHICS) && !defined(INTERACTIVE_GRAPHICS)
+				if(lbm.graphics.next_frame(lbm_T, 30.0f)) {
+					lbm.graphics.set_camera_free(float3(0.353512f*(float)Nx, -0.150326f*(float)Ny, 1.643939f*(float)Nz), -25.0f, 61.0f, 100.0f);
+					lbm.graphics.write_frame();
+				}
+		#endif // GRAPHICS && !INTERACTIVE_GRAPHICS
+	}
+}
 
-		//LBM lbm( 32u,  32u,  32u, 1.0f);
-		//LBM lbm( 64u,  64u,  64u, 1.0f);
-		//LBM lbm(128u, 128u, 128u, 1.0f);
-		LBM lbm(256u, 256u, 256u, 1.0f); // default
-		//LBM lbm(384u, 384u, 384u, 1.0f);
-		//LBM lbm(512u, 512u, 512u, 1.0f);
+// #ifdef BENCHMARK
+// #include "info.hpp"
+// void main_setup() { // benchmark; required extensions in defines.hpp: BENCHMARK, optionally FP16S or FP16C
+// 	// ################################################################## define simulation box size, viscosity and volume force ###################################################################
+// 	uint mlups = 0u; {
 
-		//const uint memory = 1488u; // memory occupation in MB (for multi-GPU benchmarks: make this close to as large as the GPU's VRAM capacity)
-		//const uint3 lbm_N = resolution(float3(1.0f, 1.0f, 1.0f), memory); // input: simulation box aspect ratio and VRAM occupation in MB, output: grid resolution
-		//LBM lbm(1u*lbm_N.x, 1u*lbm_N.y, 1u*lbm_N.z, 1u, 1u, 1u, 1.0f); // 1 GPU
-		//LBM lbm(2u*lbm_N.x, 1u*lbm_N.y, 1u*lbm_N.z, 2u, 1u, 1u, 1.0f); // 2 GPUs
-		//LBM lbm(2u*lbm_N.x, 2u*lbm_N.y, 1u*lbm_N.z, 2u, 2u, 1u, 1.0f); // 4 GPUs
-		//LBM lbm(2u*lbm_N.x, 2u*lbm_N.y, 2u*lbm_N.z, 2u, 2u, 2u, 1.0f); // 8 GPUs
+// 		//LBM lbm( 32u,  32u,  32u, 1.0f);
+// 		//LBM lbm( 64u,  64u,  64u, 1.0f);
+// 		//LBM lbm(128u, 128u, 128u, 1.0f);
+// 		LBM lbm(256u, 256u, 256u, 1.0f); // default
+// 		//LBM lbm(384u, 384u, 384u, 1.0f);
+// 		//LBM lbm(512u, 512u, 512u, 1.0f);
 
-		// #########################################################################################################################################################################################
-		for(uint i=0u; i<1000u; i++) {
-			lbm.run(10u, 1000u*10u);
-			mlups = max(mlups, to_uint((double)lbm.get_N()*1E-6/info.runtime_lbm_timestep_smooth));
-		}
-	} // make lbm object go out of scope to free its memory
-	print_info("Peak MLUPs/s = "+to_string(mlups));
-#if defined(_WIN32)
-	wait();
-#endif // Windows
-} /**/
-#endif // BENCHMARK
+// 		//const uint memory = 1488u; // memory occupation in MB (for multi-GPU benchmarks: make this close to as large as the GPU's VRAM capacity)
+// 		//const uint3 lbm_N = resolution(float3(1.0f, 1.0f, 1.0f), memory); // input: simulation box aspect ratio and VRAM occupation in MB, output: grid resolution
+// 		//LBM lbm(1u*lbm_N.x, 1u*lbm_N.y, 1u*lbm_N.z, 1u, 1u, 1u, 1.0f); // 1 GPU
+// 		//LBM lbm(2u*lbm_N.x, 1u*lbm_N.y, 1u*lbm_N.z, 2u, 1u, 1u, 1.0f); // 2 GPUs
+// 		//LBM lbm(2u*lbm_N.x, 2u*lbm_N.y, 1u*lbm_N.z, 2u, 2u, 1u, 1.0f); // 4 GPUs
+// 		//LBM lbm(2u*lbm_N.x, 2u*lbm_N.y, 2u*lbm_N.z, 2u, 2u, 2u, 1.0f); // 8 GPUs
+
+// 		// #########################################################################################################################################################################################
+// 		for(uint i=0u; i<1000u; i++) {
+// 			lbm.run(10u, 1000u*10u);
+// 			mlups = max(mlups, to_uint((double)lbm.get_N()*1E-6/info.runtime_lbm_timestep_smooth));
+// 		}
+// 	} // make lbm object go out of scope to free its memory
+// 	print_info("Peak MLUPs/s = "+to_string(mlups));
+// #if defined(_WIN32)
+// 	wait();
+// #endif // Windows
+// } /**/
+// #endif // BENCHMARK
 
 
 
@@ -515,40 +545,6 @@ void main_setup() { // benchmark; required extensions in defines.hpp: BENCHMARK,
 		lbm.voxelize_mesh_on_device(mesh);
 	}
 } /**/
-
-
-
-/*void main_setup() { // radial fan; required extensions in defines.hpp: FP16S, MOVING_BOUNDARIES, SUBGRID, INTERACTIVE_GRAPHICS or GRAPHICS
-	// ################################################################## define simulation box size, viscosity and volume force ###################################################################
-	const uint3 lbm_N = resolution(float3(3.0f, 3.0f, 1.0f), 181u); // input: simulation box aspect ratio and VRAM occupation in MB, output: grid resolution
-	const float lbm_Re = 100000.0f;
-	const float lbm_u = 0.1f;
-	const ulong lbm_T = 48000ull;
-	const ulong lbm_dt = 10ull;
-	LBM lbm(lbm_N, 1u, 1u, 1u, units.nu_from_Re(lbm_Re, (float)lbm_N.x, lbm_u));
-	// ###################################################################################### define geometry ######################################################################################
-	const float radius = 0.25f*(float)lbm_N.x;
-	const float3 center = float3(lbm.center().x, lbm.center().y, 0.36f*radius);
-	const float lbm_omega=lbm_u/radius, lbm_domega=lbm_omega*lbm_dt;
-	Mesh* mesh = read_stl(get_exe_path()+"../stl/FAN_Solid_Bottom.stl", lbm.size(), center, 2.0f*radius); // https://www.thingiverse.com/thing:6113/files
-	const uint Nx=lbm.get_Nx(), Ny=lbm.get_Ny(), Nz=lbm.get_Nz(); parallel_for(lbm.get_N(), [&](ulong n) { uint x=0u, y=0u, z=0u; lbm.coordinates(n, x, y, z);
-		if(x==0u||x==Nx-1u||y==0u||y==Ny-1u||z==0u) lbm.flags[n] = TYPE_S; // all non periodic
-	}); // ####################################################################### run simulation, export images and data ##########################################################################
-	lbm.graphics.visualization_modes = VIS_FLAG_LATTICE|VIS_FLAG_SURFACE|VIS_Q_CRITERION;
-	lbm.run(0u, lbm_T); // initialize simulation
-	while(lbm.get_t()<lbm_T) { // main simulation loop
-		lbm.voxelize_mesh_on_device(mesh, TYPE_S, center, float3(0.0f), float3(0.0f, 0.0f, lbm_omega));
-		lbm.run(lbm_dt, lbm_T);
-		mesh->rotate(float3x3(float3(0.0f, 0.0f, 1.0f), lbm_domega)); // rotate mesh
-#if defined(GRAPHICS) && !defined(INTERACTIVE_GRAPHICS)
-		if(lbm.graphics.next_frame(lbm_T, 30.0f)) {
-			lbm.graphics.set_camera_free(float3(0.353512f*(float)Nx, -0.150326f*(float)Ny, 1.643939f*(float)Nz), -25.0f, 61.0f, 100.0f);
-			lbm.graphics.write_frame();
-		}
-#endif // GRAPHICS && !INTERACTIVE_GRAPHICS
-	}
-} /**/
-
 
 
 /*void main_setup() { // electric ducted fan (EDF); required extensions in defines.hpp: FP16S, EQUILIBRIUM_BOUNDARIES, MOVING_BOUNDARIES, SUBGRID, INTERACTIVE_GRAPHICS or GRAPHICS
